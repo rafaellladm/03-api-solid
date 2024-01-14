032246dee480580fc194e674f31df56a3a24e628
Rafael Luz <rafaellladm@gmail.com>
2024-01-14 12:15:17 -0300

    feat: Check-in use case

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-ins-reposotory'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInRepository
+let sut: CheckInUseCase
+
+describe('Check-in Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gyn-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
